7a9b6505abd5a442e7b172e3ec69bc96c4631c4b
jaffasunny <jaffer.sunny125@gmail.com>
2024-02-25 22:34:31 +0500

    base url changed --prod

--- utils/Apis.ts
+++ utils/Apis.ts
@@ -16,7 +16,7 @@ export const LoginAPI: LOGIN_API_TYPES["fnType"] = async (
 ) => {
 	try {
 		const response = await axios.post(
-			DEV_BASE_URL + "/users/login",
+			PROD_BASE_URL + "/users/login",
 			{
 				emailOrUsername,
 				password,
@@ -38,7 +38,7 @@ export const LoginAPI: LOGIN_API_TYPES["fnType"] = async (
 export const RefreshAccessTokenAPI = async (user: TUserType) => {
 	try {
 		const response = await axios.post<TRefreshTokenResponse>(
-			DEV_BASE_URL + "/users/refreshToken",
+			PROD_BASE_URL + "/users/refreshToken",
 			{
 				refreshToken: user?.data?.refreshToken,
 			},
@@ -59,7 +59,7 @@ export const RefreshAccessTokenAPI = async (user: TUserType) => {
 
 export const LogoutAPI = async (user: TUserType) => {
 	try {
-		const response = await axios.post(DEV_BASE_URL + "/users/logout", {
+		const response = await axios.post(PROD_BASE_URL + "/users/logout", {
 			headers: {
 				"Content-Type": "application/json",
 				Accept: "application/json",
@@ -82,7 +82,7 @@ export const SignupAPI: SIGNUP_API_TYPES["fnType"] = async (
 ) => {
 	try {
 		const response = await axios.post(
-			DEV_BASE_URL + "/users/register",
+			PROD_BASE_URL + "/users/register",
 			{
 				firstName,
 				lastName,
@@ -107,7 +107,7 @@ export const SignupAPI: SIGNUP_API_TYPES["fnType"] = async (
 export const GetProductAPI = async (user: TUserType) => {
 	try {
 		const response = await axios.get<TGetProductAPI>(
-			DEV_BASE_URL + "/products",
+			PROD_BASE_URL + "/products",
 			{
 				headers: {
 					"Content-Type": "application/json",
@@ -134,7 +134,7 @@ export const GetSingleProductAPI = async (
 ) => {
 	try {
 		const response = await axios.get<TGetSingleProductAPI>(
-			DEV_BASE_URL + `/products/${id}`,
+			PROD_BASE_URL + `/products/${id}`,
 			{
 				headers: {
 					"Content-Type": "application/json",
@@ -157,7 +157,7 @@ export const GetSingleProductAPI = async (
 
 export const GetCartAPI = async (user: TUserType) => {
 	try {
-		const response = await axios.get(DEV_BASE_URL + "/carts", {
+		const response = await axios.get(PROD_BASE_URL + "/carts", {
 			headers: {
 				"Content-Type": "application/json",
 				Accept: "application/json",
@@ -181,7 +181,7 @@ export const AddToCartAPI = async (
 	body: TAddToCartAPIBody
 ) => {
 	try {
-		const response = await axios.post(DEV_BASE_URL + "/carts", body, {
+		const response = await axios.post(PROD_BASE_URL + "/carts", body, {
 			headers: {
 				"Content-Type": "application/json",
 				Accept: "application/json",
@@ -205,13 +205,16 @@ export const RemoveItemFromCartApi = async (
 	cartItemId: string
 ) => {
 	try {
-		const response = await axios.delete(DEV_BASE_URL + "/carts/" + cartItemId, {
-			headers: {
-				"Content-Type": "application/json",
-				Accept: "application/json",
-				Authorization: `Bearer ${user.data.accessToken}`,
-			},
-		});
+		const response = await axios.delete(
+			PROD_BASE_URL + "/carts/" + cartItemId,
+			{
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+					Authorization: `Bearer ${user.data.accessToken}`,
+				},
+			}
+		);
 
 		return response.data;
 	} catch (error: any) {
